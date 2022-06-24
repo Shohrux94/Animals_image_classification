873c22f3a8e89c7edf67904ea0e64b061f59720c
Shohrux94 <shkomoliddinov2909@gmail.com>
2022-06-24 09:19:04 +0500

    project files have been uploaded

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from fastai.vision.all import *
+import plotly.express as px
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+
+  
+
+   
+
+#title
+st.title('Hayvonlarni tasniflovchi model')
+
+#rasmni joylash
+file = st.file_uploader('Rasmni yuklash', type=['png','jpg', 'jpeg', 'gif', 'svg'])
+
+if file:
+    st.image(file)
+#PIL convert
+    img= PILImage.create(file)
+
+#model
+    model = load_learner('animals_model.pkl')
+
+#prediction
+    pred, pred_id, probs = model.predict(img)
+    st.success(f"Bashorat: {pred}")
+    st.info(f"Ehtimollik: {probs[pred_id]*100:.1f}%")
+
+    #plotting
+    fig = px.bar(x=probs*100, y=model.dls.vocab)
+    st.plotly_chart(fig)
